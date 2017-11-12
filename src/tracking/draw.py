fixed weighting scheme and sampling

src/tracking/draw.py
@@ -5,25 +5,42 @@ from tracking import cv_color
 from tracking import bcell
 
 
-def display_particle(image: np.array, particle: np.array, color: Tuple[int, int, int]):
-    display_cell(image, particle, color)
+def display_particle(image: np.array, particle: np.array):
+    center = (particle[bcell.X_POS_INDEX].astype("int32"), particle[bcell.Y_POS_INDEX].astype("int32"))
+    cv2.circle(image, center, bcell.PARTICLE_RADIUS, bcell.PARTICLE_COLOR, thickness=bcell.DEFAULT_THICKNESS)
 
 
-def display_cell(image: np.array, cell: np.array, cell_color: Tuple[int, int, int]):
-    center = tuple(cell[bcell.FIRST_POS_INDEX:bcell.LAST_POS_INDEX].astype("int32").tolist())
+def display_prediction(image: np.array, prediction: np.array):
+    center = (prediction[bcell.X_POS_INDEX].astype("int32"), prediction[bcell.Y_POS_INDEX].astype("int32"))
+
+    cv2.circle(image, center, bcell.PREDICTION_RADIUS, bcell.PREDICTION_COLOR, thickness=bcell.DEFAULT_THICKNESS)
+
+
+def display_cell(image: np.array, cell: np.array):
+    center = (cell[bcell.X_POS_INDEX].astype("int32"), cell[bcell.Y_POS_INDEX].astype("int32"))
     radius = cell[bcell.RADIUS_INDEX].astype("int32")
 
-    cv2.circle(image, center, radius, cell_color, thickness=bcell.DEFAULT_THICKNESS)
+    cv2.circle(image, center, radius, bcell.CELL_COLOR, thickness=bcell.DEFAULT_THICKNESS)
 
 
-def display_particles(image: np.array, particles: np.array,  color: Tuple[int, int, int]):
+def display_particles(image: np.array, particles: np.array):
     for particle in particles:
-        display_particle(image, particle, color)
+        display_particle(image, particle)
 
 
-def display_cells(image: np.array, cells: np.array, color: Tuple[int, int, int]):
+def display_cells(image: np.array, cells: np.array):
     for cell in cells:
-        display_cell(image, cell, color)
+        display_cell(image, cell)
+
+
+def display_predictions(image: np.array, predictions: np.array):
+    for prediction in predictions:
+        display_prediction(image, prediction)
+
+
+def display_particles_all(image: np.array, particles_all: np.array):
+    for particles in particles_all:
+        display_particles(image, particles)
 
 
 def display_cell_contours(image: np.array, cell_contours: List[np.array], cell_idx: int):
@@ -33,7 +50,7 @@ def display_cell_contours(image: np.array, cell_contours: List[np.array], cell_i
 def display_rect_bounds(image: np.array, cell_contours: List[np.array], cell_idx: int=-1):
     def display_bound(contour: np.array):
         x, y, width, height = cv2.boundingRect(contour)
-        cv2.rectangle(image, (x, y), (x + width, y + height), cv_color.GREEN, thickness=2)
+        cv2.rectangle(image, (x, y), (x + width, y + height), cv_color.GREEN, thickness=bcell.DEFAULT_THICKNESS)
 
     if cell_idx == -1:
         for cell_contour in cell_contours:
@@ -50,7 +67,7 @@ def display_circle_bounds(image: np.array, cell_contours: List[np.array], cell_i
         radius = int(radius)
 
         # show circle bound on cell
-        cv2.circle(image, center, radius, cv_color.GREEN, thickness=2)
+        cv2.circle(image, center, radius, cv_color.GREEN, thickness=bcell.DEFAULT_THICKNESS)
 
     if cell_idx == -1:
         for cell_contour in cell_contours:

src/tracking/filter.py
@@ -1,3 +1,86 @@
-class ParticleFilter:
-    def __init__(self):
-        pass
+import cv2
+import numpy as np
+
+from sklearn.utils.linear_assignment_ import _hungarian
+
+from tracking import bcell
+
+from tracking.detector import register_cells
+from tracking.draw import display_cells, display_predictions
+from tracking.image import preprocess_image, show_image
+from tracking.motion import many_to_one_dist, get_velocities, move_particles
+from tracking.reader import read_image
+from tracking.state import initialize_state, update_state, resample, get_dists_from_landmarks
+
+
+NUM_PARTICLES = 100
+
+
+def is_empty(array: np.array):
+    return True if array.shape[0] == 0 else False
+
+
+def expected_value(particles_all):
+    x_pos = particles_all[:, :, bcell.X_POS_INDEX]
+    y_pos = particles_all[:, :, bcell.Y_POS_INDEX]
+
+    weights = particles_all[:, :, bcell.WEIGHT_INDEX]
+
+    x_avg = np.average(x_pos, weights=weights, axis=1).astype("int32")
+    x_avg = x_avg.reshape((x_avg.shape[0], 1))
+
+    y_avg = np.average(y_pos, weights=weights, axis=1).astype("int32")
+    y_avg = y_avg.reshape((y_avg.shape[0], 1))
+
+    return np.hstack((x_avg, y_avg))
+
+
+def track_cells():
+    prev_cells = np.zeros((0,))
+    particles_all = np.zeros((0,))
+
+    display1 = read_image(1)
+    display2 = cv2.cvtColor(display1, cv2.COLOR_GRAY2BGR)
+
+    for i in range(10):
+        original = read_image(i + 1)
+        smoothed, contours = preprocess_image(original)
+
+        cells = register_cells(contours)
+
+        if is_empty(prev_cells):
+            prev_cells = cells
+            particles_all = initialize_state(smoothed.shape, cells, num_particles=NUM_PARTICLES)
+
+            continue
+
+        cost_matrix = np.array([many_to_one_dist(prev_cells, cell) for cell in cells])
+
+        # current cells, previous cells
+        assignments = np.array(_hungarian(cost_matrix))
+
+        prev_cells = prev_cells[assignments[:, 1]]
+        particles_all = particles_all[assignments[:, 1]]
+
+        velocities = get_velocities(prev_cells, cells)
+        velocity_noise = np.random.randn(particles_all.shape[0], particles_all.shape[1], bcell.END_POS_INDEX) * 30
+
+        move_particles(particles_all, velocities, velocity_noise)
+
+        observations_all = get_dists_from_landmarks(smoothed, cells)
+        observation_noise = 200
+
+        update_state(smoothed, particles_all, observations_all, observation_noise)
+
+        particles_all = resample(particles_all)
+        mean_centers = expected_value(particles_all)
+
+        canvas = cv2.cvtColor(smoothed, cv2.COLOR_GRAY2BGR)
+
+        # display_particles_all(canvas, particles_all)
+        display_predictions(canvas, mean_centers)
+        display_cells(canvas, cells)
+
+        show_image("smoothed image", canvas)
+
+        prev_cells = cells

src/tracking/image.py
@@ -15,6 +15,7 @@ def show_image(name: str, image: np.array):
     """
     cv2.namedWindow(name, cv2.WINDOW_NORMAL)
     cv2.resizeWindow(name, 800, 800)
+    cv2.moveWindow(name, 20, 20)
 
     cv2.imshow(name, image)
     cv2.waitKey(0)
@@ -40,7 +41,7 @@ def preprocess_image(image: np.array):
     # create contours of cells in binarized image
     contoured, contours, _ = cv2.findContours(threshed, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-    return cropped, smoothed, threshed, contoured, contours
+    return smoothed, contours
 
 
 def smooth_image(image: np.array):
@@ -57,7 +58,7 @@ def smooth_image(image: np.array):
     return avg_blurred
 
 
-def get_image_corners(image: np.array):
+def get_image_corners(image: np.array) -> np.array:
     """
     Returns four corners of 2D image.
     1st +-------------+ 4th

src/tracking/motion.py
@@ -2,15 +2,17 @@ import numpy as np
 from tracking import bcell
 
 
-def many_to_one_dist(particles: np.array, cell: np.array) -> np.array:
-    cell_vec_field = np.tile(cell[bcell.FIRST_POS_INDEX:bcell.LAST_POS_INDEX], (particles.shape[bcell.X_POS_INDEX], 1))
-    particle_centers = particles[:, bcell.FIRST_POS_INDEX:bcell.LAST_POS_INDEX]
+def many_to_one_dist(many: np.array, one: np.array) -> np.array:
+    cell_vec_field = np.tile(one[bcell.BEG_POS_INDEX:bcell.END_POS_INDEX], (many.shape[bcell.X_POS_INDEX], 1))
+    particle_centers = many[:, bcell.BEG_POS_INDEX:bcell.END_POS_INDEX]
 
     return np.linalg.norm(particle_centers - cell_vec_field, axis=1).astype("float32")
 
 
-def move_particles(particles: np.array, move_vec: np.array) -> np.array:
-    move_vec_field = np.tile(move_vec, (particles.shape[bcell.X_POS_INDEX], 1))
-    particle_centers = particles[:, bcell.FIRST_POS_INDEX:bcell.LAST_POS_INDEX]
+def get_velocities(prev_cells: np.array, curr_cells: np.array):
+    return curr_cells[:, bcell.BEG_POS_INDEX:bcell.END_POS_INDEX] - prev_cells[:, bcell.BEG_POS_INDEX:bcell.END_POS_INDEX]
 
-    particle_centers += move_vec_field
+
+def move_particles(particles: np.array, velocities: np.array, noise: np.array):
+    noisy_velocities = velocities.reshape(velocities.shape[0], 1, velocities.shape[1]) + noise
+    particles[:, :, bcell.BEG_POS_INDEX:bcell.END_POS_INDEX] += noisy_velocities

src/tracking/state.py
@@ -1,51 +1,77 @@
 import numpy as np
-from scipy.stats import norm
+from scipy import stats
+
 from tracking import bcell
-from tracking.motion import many_to_one_dist
+
+from tracking.image import get_image_corners
 
 
-WEIGHT_THRESHOLD = 0.38
+def initialize_state(image_dim: np.array, cells: np.array, num_particles: int):
+    total_num_particles = num_particles * cells.shape[0]
 
+    X = np.random.random_integers(0, image_dim[0], (total_num_particles,))
+    Y = np.random.random_integers(0, image_dim[1], (total_num_particles,))
 
-def initial_state(image_dim: np.array, cell: np.array, num_particles: int):
-    X = np.random.random_integers(0, image_dim[0], (num_particles,))
-    Y = np.random.random_integers(0, image_dim[1], (num_particles,))
+    radii = np.ones((num_particles, cells.shape[0])) * np.tile(cells[:, bcell.RADIUS_INDEX], (num_particles, 1))
+    radii = radii.T.reshape((total_num_particles,))
 
-    radii = np.ones((num_particles,)) * cell[bcell.RADIUS_INDEX]
-    weights = np.zeros((num_particles,))
+    weights = np.zeros((total_num_particles,))
 
     particles = [X, Y, radii, weights]
 
-    return np.array(particles).T.astype("float32")
+    return np.array(particles).T.reshape((cells.shape[0], num_particles, len(particles))).astype("float32")
 
 
-def update_state(particles: np.array, curr_cell: np.array):
-    normal_distro = norm()
+def update_state(image: np.array, particles_all: np.array, observations_all, noise):
+    particles_all[:, :, bcell.WEIGHT_INDEX] = 1
 
-    cell_magnitude = np.linalg.norm(curr_cell[bcell.FIRST_POS_INDEX:bcell.LAST_POS_INDEX], axis=0)
-    dists = many_to_one_dist(particles, curr_cell)
+    for particles, observations in zip(particles_all, observations_all):
+        weights = particles[:, bcell.WEIGHT_INDEX]
 
-    new_weights = [normal_distro.pdf(dist / cell_magnitude) for dist in dists]
-    particles[:, bcell.WEIGHT_INDEX] = np.array(new_weights).astype("float32")
+        particle_dists = get_dists_from_landmarks(image, particles)
 
+        preds = stats.norm(particle_dists, noise).pdf(observations)
+        weights *= np.prod(preds, axis=1).astype("float32")
 
-def prune_particles(particles: np.array, threshold: float=WEIGHT_THRESHOLD):
-    return particles[particles[:, bcell.WEIGHT_INDEX] >= threshold, :]
-
-
-def find_mean_particle(particles: np.array):
-    position = np.mean(particles[:, bcell.FIRST_POS_INDEX:bcell.LAST_POS_INDEX], axis=0)
-    return np.concatenate([position, particles[0, bcell.RADIUS_INDEX:bcell.WEIGHT_INDEX]])
+        weights /= np.sum(weights)
 
 
-def populate_particles(pruned_particles: np.array, num_particles: int):
-    mean = find_mean_particle(pruned_particles)[bcell.FIRST_POS_INDEX:bcell.LAST_POS_INDEX]
-    covar = np.cov(pruned_particles[:, bcell.X_POS_INDEX], pruned_particles[:, bcell.Y_POS_INDEX])
+def get_dists_from_landmarks(image: np.array, cells: np.array):
+    corners = get_image_corners(image)
 
-    positions = np.random.multivariate_normal(mean, covar, size=num_particles)
+    corners_mat = np.tile(corners, (cells.shape[0], 1))
+    corners_mat = corners_mat.reshape((cells.shape[0], corners.shape[0], -1))
 
-    radii = np.ones((num_particles, 1)) * pruned_particles[0, bcell.RADIUS_INDEX]
-    weights = np.zeros((num_particles, 1))
+    cells_mat = np.tile(cells[:, bcell.BEG_POS_INDEX:bcell.END_POS_INDEX], (corners.shape[0], 1, 1))
+    cells_mat = np.swapaxes(cells_mat, 0, 1)
+    cells_mat = cells_mat.reshape((cells.shape[0], corners.shape[0], -1))
 
-    return np.concatenate([positions, radii, weights], axis=1)
+    return np.linalg.norm(corners_mat - cells_mat, axis=2)
 
+
+def resample(particles_all: np.array):
+    new_particles_all = []
+
+    for particles in particles_all:
+        num_particles = particles.shape[0]
+        weights = particles[:, bcell.WEIGHT_INDEX]
+
+        marks = (np.random.rand() + np.arange(num_particles)) / num_particles
+        weights_cdf = np.cumsum(weights)
+
+        new_particles = np.zeros(particles.shape).astype("float32")
+
+        i, j = 0, 0
+        while i < num_particles:
+            if marks[i] < weights_cdf[j]:
+                new_particles[i] = particles[j]
+                i += 1
+            else:
+                j += 1
+
+        new_weights = new_particles[:, bcell.WEIGHT_INDEX]
+        new_weights /= np.sum(new_weights)
+
+        new_particles_all.append(new_particles)
+
+    return np.array(new_particles_all).astype("float32")