23b7aec3595db97ad10524c849f3f96a9d8cf9ea
ezhou7 <ezhou7@users.noreply.github.com>
2017-12-05 00:24:39 -0500

    Delete path_util.py

--- src/tracking/path_util.py
+++
@@ -1,7 +0,0 @@
-class PathUtil:
-    @staticmethod
-    def pad_zeros(num: int, max_digit_len: int):
-        str_num = str(num)
-        diff = max_digit_len - len(str_num)
-
-        return "0" * diff + str_num
